f4283c7124bebaee4f4a5a81f75344f0ce4b80a7
vazquezea96 <vazquezeduardo1278@gmail.com>
2024-02-06 16:33:17 -0800

    Nest.js set up, postgresdb connected

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import { Box, Button, Flex, Text, Input } from "@chakra-ui/react";
+import axios from "axios";
+
+type NameObject = {
+  name: string;
+};
 
 function App() {
+  const [names, setNames] = React.useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:3000/names").then((response) => {
+      setNames(response.data);
+    });
+  }, []);
+
+  const handleClick = async () => {
+    const response = await axios.post("http://localhost:3000/name", {
+      name: "John Cena",
+    });
+    console.log("RESPONSE", response);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Flex>
+      <Input placeholder="Type in your name..." />
+    </Flex>
+      <Button colorScheme="Blue" onClick={handleClick}>
+        Click Here...
+      </Button>
+      {names.map((name: NameObject) => {
+        return <Text>{name?.name}</Text>;
+      })};
+    </>
   );
 }
 
